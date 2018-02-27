472935193aee0c0309bd24de0654adbf8e6afe01
Peter Sellars <peter.sellars@hypr.co.nz>
2018-02-27 22:00:49 +1300

    remove v from hugoserver scripts hugo version

--- hugoserver.ps1
+++ hugoserver.ps1
@@ -5,4 +5,4 @@ Write-Host $MyPath
 
 docker stop hugo-server
 docker rm hugo-server
-docker run -p 1313:1313 -v ${MyPath}:/site --name hugo-server devopsdays/docker-hugo-server:v0.36.1
+docker run -p 1313:1313 -v ${MyPath}:/site --name hugo-server devopsdays/docker-hugo-server:0.36.1
